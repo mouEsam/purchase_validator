9ab37960cbc29985d5b8a8f51581143888e84b2c
Moustafa Essam <drsh_201122@yahoo.com>
2021-01-05 15:51:34 +0200

    Initial Commit

--- lib/purchase_validator.dart
+++ lib/purchase_validator.dart
@@ -2,4 +2,5 @@ library purchase_validator;
 
 export './src/models/apple/apple_validation_result.dart';
 export './src/models/google/google_validation_result.dart';
+export './src/models/validation_result.dart';
 export './src/purchase_validator.dart';
